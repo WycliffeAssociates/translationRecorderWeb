6d1ec21cb67e21cf2d233f3e61fc657db795c81a
Vicky Leong <vleong2332@gmail.com>
2016-06-21 16:58:10 -0400

    Re-design with mobile-first principle

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+	
+	// Toggle menu when clicking the menu icon
+	document.querySelector('.menuIcon').addEventListener('click', toggleMenu);
+	// Close menu when clicking on the menu links
+	document.querySelectorAll('.menu span a').forEach(function(a) {
+		a.addEventListener('click', hideMenu);
+	});
+	
+});
+
+function showMenu() {
+	document.querySelector('.menu').classList.add('show');
+	document.querySelector('.menuIcon_close').style.display = 'block';
+	document.querySelector('.menuIcon_menu').style.display = 'none';
+	document.addEventListener('click', dismissMenu);
+}
+
+function hideMenu() {
+	document.querySelector('.menu').classList.remove('show');
+	document.querySelector('.menuIcon_close').style.display = 'none';
+	document.querySelector('.menuIcon_menu').style.display = 'block';
+	document.removeEventListener('click', dismissMenu);
+}
+
+function toggleMenu() {
+	document.querySelector('.menu').classList.contains('show') ? hideMenu() : showMenu();
+}
+
+function dismissMenu (e) {
+	!closest(e.target, 'nav') && hideMenu();
+}
+
+// get nearest parent element matching selector
+function closest(el, selector) {
+    var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+    while (el) {
+        if (matchesSelector.call(el, selector)) {
+            break;
+        }
+        el = el.parentElement;
+    }
+    return el;
+}
