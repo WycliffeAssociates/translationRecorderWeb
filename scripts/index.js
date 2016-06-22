5e9509ccdfee68eee22bd653977f5199d7253106
Vicky Leong <vleong2332@gmail.com>
2016-06-22 09:36:48 -0400

    Make banner full-width on wide screen

--- scripts/index.js
+++ scripts/index.js
@@ -30,16 +30,3 @@ function toggleMenu() {
 function dismissMenu (e) {
 	!closest(e.target, 'nav') && hideMenu();
 }
-
-// get nearest parent element matching selector
-function closest(el, selector) {
-    var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
-
-    while (el) {
-        if (matchesSelector.call(el, selector)) {
-            break;
-        }
-        el = el.parentElement;
-    }
-    return el;
-}
---
+++ scripts/utils.js
@@ -0,0 +1,13 @@
+// Get nearest parent element matching selector
+function closest(el, selector) {
+    var matchesSelector = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector || el.msMatchesSelector;
+
+    while (el) {
+        if (matchesSelector.call(el, selector)) {
+            break;
+        }
+        el = el.parentElement;
+    }
+    
+    return el;
+}
